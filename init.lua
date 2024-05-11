a10d4fdf9e9d23d42d3e8c8689f616e856de6afd
TheNewLearner1 <zzzooozzz836@gmail.com>
2024-05-11 16:26:19 +0545

    added treesitter / telescope

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = " "
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -11,9 +13,29 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 local plugins = {
-{ "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+--                         , branch = '0.1.x',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
 }
 local opts = {}
+
 require("lazy").setup(plugins, opts)
-require("catppuccin").setup()
+local builtin = require("telescope.builtin")
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+    ensure_installed = {"python", "go", "lua", "rust", "vim"},
+    highlight = {enable = true},
+    indent = {enable = true},
+
+})
+
 vim.cmd.colorscheme "lunaperche"
+
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
