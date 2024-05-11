a142de85d73727d936f13703659f4d05c5df6651
TheNewLearner1 <zzzooozzz836@gmail.com>
2024-05-11 19:15:07 +0545

    fixed neotree due to :neotree instead of :Neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -8,7 +8,7 @@ return {
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
      config = function()
-	vim.keymap.set('n', '<leader>e', ':neotree')
+	vim.keymap.set('n', '<leader>e', ':Neotree')
 	end
 }
 
