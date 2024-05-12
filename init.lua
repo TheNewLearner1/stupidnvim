c90e117a36861e344b68b9a0160c6f008fdc87ed
TheNewLearner1 <zzzooozzz836@gmail.com>
2024-05-12 23:01:36 +0545

    !

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.g.swapfile = false
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
