194f046ebde5ce7b329e64d8bc189f7e78fa405a
TheNewLearner1 <zzzooozzz836@gmail.com>
2024-05-12 16:27:53 +0545

    updated

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -17,7 +17,7 @@ return {
 	},
 	
 	{
- 	"neovim/nvim-lspconfig",
+  	"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({})
@@ -26,7 +26,7 @@ return {
 			lspconfig.gopls.setup({})
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-			vim.keymap.set('b',  '<leader>ca', vim.lsp.buf.code_action, {})
+			vim.keymap.set('n',  '<leader>ca', vim.lsp.buf.code_action, {})
 		end
 	}
 
