a932fbb2ee0c38651c125b26f767821624910ab7
TheNewLearner1 <zzzooozzz836@gmail.com>
2024-05-12 18:41:58 +0545

    formatting using none-ls

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,6 +5,7 @@ return {
 	local config = require("nvim-treesitter.configs")
 		config.setup({
     		ensure_installed = {"python", "go", "lua", "rust", "vim"},
+		auto_install = true,
     		highlight = {enable = true},
     		indent = {enable = true},
 		})
