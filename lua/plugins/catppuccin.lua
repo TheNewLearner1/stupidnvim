0d62b845f71f2dcf6ad5f381c78be55a92e510d2
TheNewLearner1 <zzzooozzz836@gmail.com>
2024-05-13 18:32:10 +0545

    changed theme

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,9 +1,7 @@
  return { "catppuccin/nvim",
 	 name = "catppuccin",
 	 priority = 1000,
--- if you want to use this theme
---	 config = function()
---	    vim.cmd.colorscheme "catppuccin"
---       end
-  vim.cmd.colorscheme "torte"
+	 config = function()
+	    vim.cmd.colorscheme "catppuccin"
+       end
  } 
