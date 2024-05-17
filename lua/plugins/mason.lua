3dd97a954bc9b0225a54649d925a5d3b04966c0e
TheNewLearner1 <zzzooozzz836@gmail.com>
2024-05-17 23:13:17 +0545

    update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,38 +1,42 @@
 return {
   {
-    require("williamboman/mason.nvim"),
-    lazy = false, 
+    "williamboman/mason.nvim",
+    lazy = false,
     config = function()
       require("mason").setup()
     end,
   },
   {
-    require("williamboman/maosn-lspconfig.nvim"),
+    "williamboman/mason-lspconfig.nvim",
     lazy = false,
     opts = {
       auto_install = true,
     },
   },
   {
-    require("neovim/lsp-config"), 
-    lazy = false, 
+    "neovim/nvim-lspconfig",
+    lazy = false,
     config = function()
-      local capabilities = require("cmp_nvim_lsp").dafault_capabilities()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
       local lspconfig = require("lspconfig")
-      lspconfig.rust_analyzer.setup({ capabilites = capabilities})
-      lspconfig.lua_ls.setup({ capabilities = capabilities})
-      lspconfig.pyright.setup({ capabilities = capabilities})
-      lspconfig.gopls.setup({ capabilities = capabilities})
-      
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', '<leader>gr', vim.lsp.buf.reference, {})
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+      lspconfig.tsserver.setup({
+        capabilities = capabilities
+      })
+      lspconfig.solargraph.setup({
+        capabilities = capabilities
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
 
-
-      
-    end
-
-
-    },
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,5 +5,6 @@ return {
   config = function()
     local builtin = require("telescope.builtin")
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
   end
 }
