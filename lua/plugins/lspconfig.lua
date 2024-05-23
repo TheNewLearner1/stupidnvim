be899f3102ef4f84e674e6f9d78a3300a00ec372
bob <zzzooozzz836@gmail.com>
2024-05-23 21:46:11 +0545

    added lsp going to read and try to understand lspsetup

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,24 @@
+return {
+    'junnplus/lsp-setup.nvim',
+    dependencies = {
+        'neovim/nvim-lspconfig',
+        'williamboman/mason.nvim', -- optional
+        'williamboman/mason-lspconfig.nvim', -- optional
+    },
+  config = function()
+    require('lsp-setup').setup({
+    servers = {
+        pyright = {},
+        clangd = {},
+        lua_ls = {},
+        rust_analyzer = {},
+    },
+
+    inlay_hints = {
+        enabled = true,
+    },
+      
+    })
+  end
+
+  }
